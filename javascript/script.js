0b12db9fb60a5263b13ba947bc0099cdf8a6f0ad
Caleb <dougalcaleb@gmail.com>
2021-01-25 10:36:01 -0700

    Better indentation

--- javascript/script.js
+++ javascript/script.js
@@ -3,16 +3,16 @@ const input = document.querySelector(".code");
 // Settings
 const autocomplete = [
 	["[", "]"],
-   ["(", ")"],
-   ["{", "}"],
+	["(", ")"],
+	["{", "}"],
 	[`"`, `"`],
 	["`", "`"],
-	["'", "'"]
+	["'", "'"],
 ];
 const tabPairs = [
-   ["(", ")"],
-   ["{", "}"],
-   ["[", "]"]
+	["(", ")"],
+	["{", "}"],
+	["[", "]"],
 ];
 const tabSize = 4;
 let updateSpeed = 1000;
@@ -26,8 +26,6 @@ let lastWasPair = false;
 
 // TODO:
 /*
--  Correct indentation
-   -  Detect how many pairs it's wrapped in
 -  Autocomplete
    -  Simple box matching typing
 -  Auto update / update on code close
@@ -35,49 +33,61 @@ let lastWasPair = false;
 */
 
 input.addEventListener("keydown", (e) => {
-   clearTimeout(updateChecker);
-   updateChecker = setTimeout(checkForUpdate, updateSpeed);
-   updated = false;
-   checkForChar(input, 0);
-   if (e.key == "Tab") {
-      e.preventDefault();
-      for (let a = 0; a < tabSize * tabs; a++) {
-         insertAtCursor(" ", false);
-      }
-      return;
-   }
-   if (e.key == "Enter") {
-      setTimeout(() => {
-         for (let a = 0; a < tabSize * tabs; a++) {
-            insertAtCursor(" ", false);
-         }
-      }, 0);
-      calcTabs();
-      lastWasPair = false;
-      return;
-   }
-   if (e.key == "Backspace") {
-      if (lastWasPair) {
-         e.preventDefault();
-         lastWasPair = false;
-         insertAtCursor("DELETE", false, -1);
-         insertAtCursor("DELETE", true, 0);
-      }
-   }
-   let wasPair = false;
+	clearTimeout(updateChecker);
+	updateChecker = setTimeout(checkForUpdate, updateSpeed);
+	updated = false;
+	checkForChar(input, 0);
+	if (e.key == "Tab") {
+		e.preventDefault();
+		for (let a = 0; a < tabSize; a++) {
+			insertAtCursor(" ", false);
+		}
+		return;
+	}
+	if (e.key == "Enter") {
+		setTimeout(() => {
+			let pairCorrection = 0;
+         tabPairs.forEach((char) => {
+				if (checkForChar(0, char[0])) {
+					pairCorrection = -1;
+					console.log(`Found ${char[0]}. Not tabbing.`);
+            }
+            if (checkForChar(0, char[1])) {
+					pairCorrection = -1;
+					console.log(`Found ${char[1]}. Not tabbing.`);
+				}
+			});
+
+			for (let a = 0; a < tabSize * tabs; a++) {
+				insertAtCursor(" ", false);
+			}
+		}, 0);
+
+		calcTabs(pairCorrection);
+		lastWasPair = false;
+		return;
+	}
+	if (e.key == "Backspace") {
+		if (lastWasPair) {
+			e.preventDefault();
+			lastWasPair = false;
+			insertAtCursor("DELETE", false, -1);
+			insertAtCursor("DELETE", true, 0);
+		}
+	}
+	let wasPair = false;
 	autocomplete.forEach((pair) => {
 		if (e.key == pair[0] && !checkForChar(1)) {
 			e.preventDefault();
 			insertAtCursor(pair[0], false);
-         insertAtCursor(pair[1], true);
-         lastWasPair = true;
-         wasPair = true;
-      }
-   });
-   console.log(wasPair);
-   if (!wasPair) {
-      lastWasPair = false;
-   }
+			insertAtCursor(pair[1], true);
+			lastWasPair = true;
+			wasPair = true;
+		}
+	});
+	if (!wasPair) {
+		lastWasPair = false;
+	}
 });
 
 function insertAtCursor(val, isEnd, side) {
@@ -90,78 +100,80 @@ function insertAtCursor(val, isEnd, side) {
 	//MOZILLA and others
 	else if (input.selectionStart || input.selectionStart == "0") {
 		var startPos = input.selectionStart;
-      var endPos = input.selectionEnd;
-      if (val == "DELETE") {
-         input.value = input.value.substring(0, startPos+side) + input.value.substring(endPos+side+1, input.value.length);
-      } else {
-         input.value = input.value.substring(0, startPos) + val + input.value.substring(endPos, input.value.length);
-      }
+		var endPos = input.selectionEnd;
+		if (val == "DELETE") {
+			input.value = input.value.substring(0, startPos + side) + input.value.substring(endPos + side + 1, input.value.length);
+		} else {
+			input.value = input.value.substring(0, startPos) + val + input.value.substring(endPos, input.value.length);
+		}
 		if (isEnd && val != "DELETE") {
 			input.selectionStart = startPos + val.length - 1;
 			input.selectionEnd = startPos + val.length - 1;
 		} else if (val != "DELETE") {
 			input.selectionStart = startPos + val.length;
 			input.selectionEnd = startPos + val.length;
-      } else if (val == "DELETE" && !isEnd) {
-         input.selectionStart = startPos-1;
-         input.selectionEnd = startPos-1;
-      } else if (val == "DELETE" && isEnd) {
-         input.selectionStart = startPos;
-         input.selectionEnd = startPos;
-      }
+		} else if (val == "DELETE" && !isEnd) {
+			input.selectionStart = startPos - 1;
+			input.selectionEnd = startPos - 1;
+		} else if (val == "DELETE" && isEnd) {
+			input.selectionStart = startPos;
+			input.selectionEnd = startPos;
+		}
 	} else {
 		input.value += val;
 	}
 }
 
 function checkForChar(position, value) {
-   var pos = input.selectionStart + position;
-   let char = input.value.substring(pos+position-2, pos+position-1);
-   if (!value && char.trim() != "") {
-      return true;
-   } else if (value == char) {
-      return true;
-   }
-   return false;
+	var pos = input.selectionStart + position;
+	let char = input.value.substring(pos + position - 2, pos + position - 1);
+	if (!value && char.trim() != "") {
+		return true;
+	} else if (value == char) {
+		return true;
+	}
+	return false;
 }
 
 function checkForUpdate() {
-   if (!updated) {
-      console.log("Updating");
-      carousel = null;
-      let rawSettings = input.value.trim().substring(15, input.value.trim().length - 2);
-      console.log(rawSettings);
-      let settings = JSON.parse(input.value.trim().substring(15, input.value.trim().length - 2));
-      console.log("Settings:");
-      console.log(settings);
-      let destroy = settings.id || ".myCarousel";
-      if (document.querySelector(destroy)) {
-         document.querySelector(destroy).remove();
-      }
-      if (document.querySelector(".carousel-error-message")) {
-         document.querySelector(".carousel-error-message").remove();
-      }
-      roundabout.usedIds = [];
-      roundabout.on = -1;
-      carousel = new Roundabout(settings);
-      updated = true;
-   }
+	if (!updated) {
+		carousel = null;
+		let rawSettings = input.value.trim().substring(15, input.value.trim().length - 2);
+		let settings;
+		try {
+			settings = JSON.parse(input.value.trim().substring(15, input.value.trim().length - 2));
+			document.querySelector(".code-toggle").style.color = "";
+		} catch (e) {
+			document.querySelector(".code-toggle").style.color = "red";
+		}
+		let destroy = settings.id || ".myCarousel";
+		if (document.querySelector(destroy)) {
+			document.querySelector(destroy).remove();
+		}
+		if (document.querySelector(".carousel-error-message")) {
+			document.querySelector(".carousel-error-message").remove();
+		}
+		roundabout.usedIds = [];
+		roundabout.on = -1;
+		carousel = new Roundabout(settings);
+		updated = true;
+	}
 }
 
-function calcTabs() {
-   let chars = input.value.slice(0, input.selectionStart).split("");
-   let pairs = 0;
-   chars.forEach((codeChar) => {
-      tabPairs.forEach((tabChar) => {
-         if (codeChar == tabChar[0]) {
-            pairs++;
-         }
-         if (codeChar == tabChar[1]) {
-            pairs--;
-         }
-      });
-   });
-   tabs = pairs - 1;
+function calcTabs(correction) {
+	let chars = input.value.slice(0, input.selectionStart).split("");
+	let pairs = 0;
+	chars.forEach((codeChar) => {
+		tabPairs.forEach((tabChar) => {
+			if (codeChar == tabChar[0]) {
+				pairs++;
+			}
+			if (codeChar == tabChar[1]) {
+				pairs--;
+			}
+		});
+	});
+	tabs = pairs - 1 + correction;
 }
 
 updateChecker = setTimeout(checkForUpdate, updateSpeed);
@@ -187,11 +199,11 @@ new Roundabout({
 */
 
 document.querySelector(".code-toggle").addEventListener("click", () => {
-   if (codeShown) {
-      input.style.display = "none";
-      codeShown = false;
-   } else {
-      input.style.display = "inline-block";
-      codeShown = true;
-   }
+	if (codeShown) {
+		input.style.display = "none";
+		codeShown = false;
+	} else {
+		input.style.display = "inline-block";
+		codeShown = true;
+	}
 });
